28386ba50c296f731e67ca0da7ec537996a7e839
C.P. Mohammed Sadiq Ali <22ad064@excelcolleges.com>
2024-08-21 00:02:20 +0530

    Update urls.py

--- bookproject/urls.py
+++ bookproject/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookadmin/',include('bookapp.urls')),
-    path('user/',include('userapp.urls'))
+    path('',include('userapp.urls'))
 ]
 
 if settings.DEBUG:
