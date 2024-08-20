a223f5987ecad5dfdb1ca7ad173ba62a7f361242
C.P. Mohammed Sadiq Ali <22ad064@excelcolleges.com>
2024-08-20 23:57:01 +0530

    Update models.py

--- userapp/models.py
+++ userapp/models.py
@@ -3,7 +3,8 @@ from bookapp.models import book
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from .managers import CustomClassManager
+from django.contrib.auth.models import User
+# from .managers import CustomClassManager
 
 # Create your models here.
 
@@ -26,28 +27,31 @@ from .managers import CustomClassManager
 
 #     def __str__(self) -> str:
 #         return "{}".format(self.username)
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_("email address"), unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
+# class CustomUser(AbstractBaseUser, PermissionsMixin):
+#     email = models.EmailField(_("email address"), unique=True)
+#     is_staff = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=True)
+#     date_joined = models.DateTimeField(default=timezone.now)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = []
 
-    objects = CustomClassManager()
+#     # objects = CustomClassManager()
 
-    def __str__(self):
-        return self.email
+#     def __str__(self):
+#         return self.email
 
 
 class Cart(models.Model):
-    user = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(book)
 
+# class CartItem(models.Model):
+#     cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+#     Book = models.ForeignKey(book,on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField(default=1)
+
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
-    Book = models.ForeignKey(book,on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    Book = models.ForeignKey(book, on_delete=models.CASCADE)  # Assuming you have a Book model
     quantity = models.PositiveIntegerField(default=1)
-
-
