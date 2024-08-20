f5dd1682e2c5042c19f2d17a67c0f69ad3dcae6b
C.P. Mohammed Sadiq Ali <22ad064@excelcolleges.com>
2024-08-20 23:55:03 +0530

    Update urls.py

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     path("create-book/",views.createBook,name="createbook"),
-    path("",views.listBook,name="booklist"),
+    path("booklist/",views.listBook,name="booklist"),
     path("adbook/<int:book_id>",views.detailsview,name="bookdetails"),
     path("updateview/<int:book_id>",views.updateBook,name="update"),
     path("delete/<int:book_id>",views.deleteView,name="delete"),
     path("authorview/",views.CreateAuthor,name="author"),
     path("index/",views.index),
     path("search/", views.searchBook,name="search"),
-    path("signup/",views.registerUser,name="signup"),
-    path("login/", views.loginUser,name="login"),
-    path("logout",views.logOut,name="logout"),
+    path("signup",views.registerUser,name="signup"),
+    path("", views.loginUser,name="login"),
+    path('logout/', views.logOut_user, name='logout'),
 ]
